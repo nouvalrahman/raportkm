90be476b37f82d2044cbab7469b4e57f18f457b3
nouvalrahman <nouvalrahman28@gmail.com>
2023-04-05 23:00:55 +0700

    fix bug

--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -4,121 +4,121 @@
             <img src="<?= base_url('assets/img/smamita.png') ?>" alt="" class="img-fluid mb-2">
         </a>
     </div>
-    <?php foreach ($menu as $m): ?>
 
 
-        <ul class="nav-links">
-            <li>
-                <a href="#">
-                    <i class='bx bx-home-circle'></i>
-                    <span class="link_name">Dashboard</span>
-                </a>
-                <ul class="sub-menu blank">
-                    <li><a class="link_name" href="">Menu</a></li>
-                </ul>
-            </li>
-            <li>
-                <div class="iocn-link">
-                    <a href="#">
-                        <i class='bx bx-lock-open-alt'></i>
-                        <span class="link_name">Access</span>
-                    </a>
-                    <i class='bx bxs-chevron-down arrow'></i>
-                </div>
-                <ul class="sub-menu">
 
-                    <li><a href="<?= base_url('Menu/index') ?>">Menu</a></li>
-                    <li><a href="<?= base_url('Submenu/index') ?>">Sub Menu</a></li>
-                    <li><a href="#">Useraccess</a></li>
-                </ul>
-            </li>
-            <li>
-                <div class="iocn-link">
-                    <a href="#">
-                        <i class='bx bx-box'></i>
-                        <span class="link_name">Masters</span>
-                    </a>
-                    <i class='bx bxs-chevron-down arrow'></i>
-                </div>
-                <ul class="sub-menu">
-                    <li><a class="link_name" href="#">Posts</a></li>
-                    <li><a href="#">Identitas Sekolah</a></li>
-                    <li><a href="#">Users</a></li>
-                    <li><a href="#">Kelas</a></li>
-                    <li><a href="#">Siswa</a></li>
-                    <li><a href="#">Mata pelajaran</a></li>
-                </ul>
-            </li>
-            <li>
+    <ul class="nav-links">
+        <li>
+            <a href="#">
+                <i class='bx bx-home-circle'></i>
+                <span class="link_name">Dashboard</span>
+            </a>
+            <ul class="sub-menu blank">
+                <li><a class="link_name" href="">Menu</a></li>
+            </ul>
+        </li>
+        <li>
+            <div class="iocn-link">
                 <a href="#">
-                    <i class='bx bx-pie-chart-alt-2'></i>
-                    <span class="link_name">Analytics</span>
+                    <i class='bx bx-lock-open-alt'></i>
+                    <span class="link_name">Access</span>
                 </a>
-                <ul class="sub-menu blank">
-                    <li><a class="link_name" href="#">Analytics</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="#">
-                    <i class='bx bx-line-chart'></i>
-                    <span class="link_name">Chart</span>
-                </a>
-                <ul class="sub-menu blank">
-                    <li><a class="link_name" href="#">Chart</a></li>
-                </ul>
-            </li>
-            <li>
-                <div class="iocn-link">
-                    <a href="#">
-                        <i class='bx bx-plug'></i>
-                        <span class="link_name">Plugins</span>
-                    </a>
-                    <i class='bx bxs-chevron-down arrow'></i>
-                </div>
-                <ul class="sub-menu">
-                    <li><a class="link_name" href="#">Plugins</a></li>
-                    <li><a href="#">UI Face</a></li>
-                    <li><a href="#">Pigments</a></li>
-                    <li><a href="#">Box Icons</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="#">
-                    <i class='bx bx-compass'></i>
-                    <span class="link_name">Explore</span>
-                </a>
-                <ul class="sub-menu blank">
-                    <li><a class="link_name" href="#">Explore</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="#">
-                    <i class='bx bx-history'></i>
-                    <span class="link_name">History</span>
-                </a>
-                <ul class="sub-menu blank">
-                    <li><a class="link_name" href="#">History</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="#">
-                    <i class='bx bx-cog'></i>
-                    <span class="link_name">Setting</span>
-                </a>
-                <ul class="sub-menu blank">
-                    <li><a class="link_name" href="#">Setting</a></li>
-                </ul>
-            </li>
-            <li>
-                <div class="profile-details">
+                <i class='bx bxs-chevron-down arrow'></i>
+            </div>
+            <ul class="sub-menu">
 
-                    <div class="name-job">
-                        <i class='bx bx-log-in'></i>
-                    </div>
-                    <i class='bx bx-log-out'></i>
+                <li><a href="<?= base_url('Menu/index') ?>">Menu</a></li>
+                <li><a href="<?= base_url('Submenu/index') ?>">Sub Menu</a></li>
+                <li><a href="#">Useraccess</a></li>
+            </ul>
+        </li>
+        <li>
+            <div class="iocn-link">
+                <a href="#">
+                    <i class='bx bx-box'></i>
+                    <span class="link_name">Masters</span>
+                </a>
+                <i class='bx bxs-chevron-down arrow'></i>
+            </div>
+            <ul class="sub-menu">
+                <li><a class="link_name" href="#">Posts</a></li>
+                <li><a href="#">Identitas Sekolah</a></li>
+                <li><a href="#">Users</a></li>
+                <li><a href="#">Kelas</a></li>
+                <li><a href="#">Siswa</a></li>
+                <li><a href="#">Mata pelajaran</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="#">
+                <i class='bx bx-pie-chart-alt-2'></i>
+                <span class="link_name">Analytics</span>
+            </a>
+            <ul class="sub-menu blank">
+                <li><a class="link_name" href="#">Analytics</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="#">
+                <i class='bx bx-line-chart'></i>
+                <span class="link_name">Chart</span>
+            </a>
+            <ul class="sub-menu blank">
+                <li><a class="link_name" href="#">Chart</a></li>
+            </ul>
+        </li>
+        <li>
+            <div class="iocn-link">
+                <a href="#">
+                    <i class='bx bx-plug'></i>
+                    <span class="link_name">Plugins</span>
+                </a>
+                <i class='bx bxs-chevron-down arrow'></i>
+            </div>
+            <ul class="sub-menu">
+                <li><a class="link_name" href="#">Plugins</a></li>
+                <li><a href="#">UI Face</a></li>
+                <li><a href="#">Pigments</a></li>
+                <li><a href="#">Box Icons</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="#">
+                <i class='bx bx-compass'></i>
+                <span class="link_name">Explore</span>
+            </a>
+            <ul class="sub-menu blank">
+                <li><a class="link_name" href="#">Explore</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="#">
+                <i class='bx bx-history'></i>
+                <span class="link_name">History</span>
+            </a>
+            <ul class="sub-menu blank">
+                <li><a class="link_name" href="#">History</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="#">
+                <i class='bx bx-cog'></i>
+                <span class="link_name">Setting</span>
+            </a>
+            <ul class="sub-menu blank">
+                <li><a class="link_name" href="#">Setting</a></li>
+            </ul>
+        </li>
+        <li>
+            <div class="profile-details">
+
+                <div class="name-job">
+                    <i class='bx bx-log-in'></i>
                 </div>
-            </li>
-        <?php endforeach ?>
+                <i class='bx bx-log-out'></i>
+            </div>
+        </li>
+
     </ul>
 </div>
 <!-- <section class="home-section">
