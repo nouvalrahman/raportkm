a4cc6872634c6257d97da4141d53d21621ab9ba8
unknown <nouvalrahman@gmail.com>
2023-04-25 18:08:06 +0700

    export new db

---
+++ application/models/Jurusanmodel.php
@@ -0,0 +1,20 @@
+<?php
+
+class Jurusanmodel extends CI_model {
+
+    public function get_jurusan()
+    {
+
+    }
+
+    public function tambah()
+    {
+
+    }
+
+    public function ubah()
+    {
+
+    }
+}
+?>
